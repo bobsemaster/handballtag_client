f180bf1a3f72a8eea5aa30602930e9daf806b1c0
Robert.Schreib <robert.schreib@e-mundo.de>
2018-06-01 20:09:12 +0200

    Spiel View filterung nach platz und nach Jugend

--- src/pages/spiel-view/spiel-view.ts
+++ src/pages/spiel-view/spiel-view.ts
@@ -5,8 +5,6 @@ import {SpielServiceProvider} from "../../providers/spiel-service/spiel-service"
 import {GenerateSpielplanViewPage} from "../generate-spielplan-view/generate-spielplan-view";
 import {Spiel} from "../../models/Spiel";
 import {ApplicationDataServiceProvider} from "../../providers/application-data-service/application-data-service";
-import {Jugend} from "../../models/Jugend";
-import {Mannschaft} from '../../models/Mannschaft';
 
 /**
  * Generated class for the SpielViewPage page.
@@ -23,6 +21,9 @@ import {Mannschaft} from '../../models/Mannschaft';
 export class SpielViewPage {
   public allSpiel: Spiel[];
   public allSpielView: SpielViewHelper[];
+
+  //Wenn true gruppiere spiele nach jugend sonst nach platz
+  public groupJugend: boolean = true;
 
   private days = ["So.", "Mo.", "Di.", "Mi.", "Do.", "Fr.", "Sa."];
 
@@ -51,26 +52,62 @@ export class SpielViewPage {
     this.spielService.getAllSpiel().subscribe(allSpiel => {
       this.allSpiel = allSpiel;
 
-      const jugendMannschaften = new Map<number, Spiel[]>();
-
-      this.allSpiel.forEach(spiel => {
-        if(jugendMannschaften.has(spiel.heimMannschaft.jugend.hashCode())){
-          jugendMannschaften.get(spiel.heimMannschaft.jugend.hashCode()).push(spiel);
-        } else {
-          jugendMannschaften.set(spiel.heimMannschaft.jugend.hashCode(), [spiel]);
-        }
-      });
-      console.log(jugendMannschaften);
-      this.allSpielView = [];
-      jugendMannschaften.forEach((allSpiel, key) => {
-        const spielView = new SpielViewHelper();
-        spielView.title = `${allSpiel[0].heimMannschaft.jugend.typ} ${allSpiel[0].heimMannschaft.jugend.jahrgang}`
-        spielView.allSpiel = allSpiel;
-        spielView.nextSpiel = allSpiel[0];
-        this.allSpielView.push(spielView);
-      })
+      this.groupByJugend();
     });
 
+  }
+
+  toggleGroupByJugend() {
+    this.groupJugend = !this.groupJugend;
+
+    if (this.groupJugend) {
+      this.groupByJugend();
+    } else {
+      this.groupByPlatz();
+    }
+  }
+
+  private groupByJugend() {
+    const jugendMannschaften = new Map<number, Spiel[]>();
+
+    this.allSpiel.forEach(spiel => {
+      if (jugendMannschaften.has(spiel.heimMannschaft.jugend.hashCode())) {
+        jugendMannschaften.get(spiel.heimMannschaft.jugend.hashCode()).push(spiel);
+      } else {
+        jugendMannschaften.set(spiel.heimMannschaft.jugend.hashCode(), [spiel]);
+      }
+    });
+    console.log(jugendMannschaften);
+    this.allSpielView = [];
+    jugendMannschaften.forEach((allSpiel, key) => {
+      const spielView = new SpielViewHelper();
+      spielView.title = `${allSpiel[0].heimMannschaft.jugend.typ} ${allSpiel[0].heimMannschaft.jugend.jahrgang}`;
+      spielView.allSpiel = allSpiel;
+      spielView.nextSpiel = allSpiel[0];
+      this.allSpielView.push(spielView);
+    })
+  }
+
+
+  private groupByPlatz() {
+    const platzMannschaften = new Map<number, Spiel[]>();
+
+    this.allSpiel.forEach(spiel => {
+      if (platzMannschaften.has(spiel.spielPlatz)) {
+        platzMannschaften.get(spiel.spielPlatz).push(spiel);
+      } else {
+        platzMannschaften.set(spiel.spielPlatz, [spiel]);
+      }
+    });
+    console.log(platzMannschaften);
+    this.allSpielView = [];
+    platzMannschaften.forEach((allSpiel, key) => {
+      const spielView = new SpielViewHelper();
+      spielView.title = `Platz ${key}`;
+      spielView.allSpiel = allSpiel;
+      spielView.nextSpiel = allSpiel[0];
+      this.allSpielView.push(spielView);
+    })
   }
 }
 
