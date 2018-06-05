d148afc166fe65a667e46cef9a688516fd2f8340
Robert.Schreib <robert.schreib@e-mundo.de>
2018-06-05 14:09:05 +0200

    Http Interceprtor um CORS für IOS zu ermöglichen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import {StatusBar} from '@ionic-native/status-bar';
 import {SplashScreen} from '@ionic-native/splash-screen';
 import {LoginPage} from "../pages/login/login";
 import {AuthenticationServiceProvider} from '../providers/authentication-service/authentication-service';
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {HttpClient, HttpClientModule, HTTP_INTERCEPTORS} from "@angular/common/http";
 import {VereinViewPageModule} from "../pages/verein-view/verein-view.module";
 import {VereinServiceProvider} from '../providers/verein-service/verein-service';
 import {ApplicationDataServiceProvider} from '../providers/application-data-service/application-data-service';
@@ -16,6 +16,7 @@ import {MannschaftenViewPageModule} from "../pages/mannschaften-view/mannschafte
 import {MannschaftServiceProvider} from '../providers/mannschaft-service/mannschaft-service';
 import {SpielServiceProvider} from '../providers/spiel-service/spiel-service';
 import {SpielViewPageModule} from "../pages/spiel-view/spiel-view.module";
+import { HttpInterceptorProvider } from '../providers/http-interceptor/http-interceptor';
 
 @NgModule({
   declarations: [
@@ -45,6 +46,11 @@ import {SpielViewPageModule} from "../pages/spiel-view/spiel-view.module";
     ApplicationDataServiceProvider,
     MannschaftServiceProvider,
     SpielServiceProvider,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpInterceptorProvider ,
+      multi: true
+    },
   ],
 
 })
