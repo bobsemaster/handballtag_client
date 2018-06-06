93a5f0956200198ee24b8704c48b83fda66451f1
iMaxbook <maxfx.andre@gmail.com>
2018-06-06 10:40:06 +0200

    Login error hinzugefügt

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {NavController, NavParams} from 'ionic-angular';
+import {AlertController, NavController, NavParams} from 'ionic-angular';
 import {AuthenticationServiceProvider} from "../../providers/authentication-service/authentication-service";
 import {VereinViewPage} from "../verein-view/verein-view";
 import {ApplicationDataServiceProvider} from "../../providers/application-data-service/application-data-service";
@@ -19,21 +19,29 @@ export class LoginPage {
 
   public loginCredentials = {username: '', password: ''};
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private auth: AuthenticationServiceProvider, private applicationData: ApplicationDataServiceProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private auth: AuthenticationServiceProvider, private applicationData: ApplicationDataServiceProvider, private alertController:AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
     this.isAlreadyAuthenticated()
   }
-
   private isAlreadyAuthenticated() {
     this.applicationData.ladeAuthentifiziertenBenutzer().add(() => this.navigateToUserStartPage());
     // Lade alle vereine
   }
 
   public login(event: any): void {
-    this.auth.authenticateUser(this.loginCredentials.username, this.loginCredentials.password).subscribe(success => {
+    this.auth.authenticateUser(this.loginCredentials.username, this.loginCredentials.password)
+      .catch((err:any, caught:any) => {
+        this.alertController.create({
+          title: 'Login Fehlgeschlagen',
+          message: 'Username oder passwort falsch!',
+
+        }).addButton("Ok").present();
+        return [];
+      })
+      .subscribe(success => {
       //Wenn null zurückkommt ist die authentifizierung erfolgreich sonst kommt ein error Zurück
       if (success == null) {
         this.isAlreadyAuthenticated();
