5b71f403e8407d40dc85ac68cd0d53b24da88a98
Robert.Schreib <robert.schreib@e-mundo.de>
2018-07-12 22:22:43 +0200

    Has Foto Page Hinzugefügt

---
+++ src/pages/has-foto/has-foto.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+import {IonicPageModule} from 'ionic-angular';
+import {HasFotoPage} from './has-foto';
+
+@NgModule({
+  declarations: [
+    HasFotoPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(HasFotoPage),
+  ],
+})
+export class HasFotoPageModule {
+}
