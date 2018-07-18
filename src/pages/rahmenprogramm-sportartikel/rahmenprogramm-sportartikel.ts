7e3de02556931838d4ab296fd7b0d9998fd1ecbe
robert <ros0721@thi.de>
2018-07-18 20:24:21 +0200

    Version auf 1.0.10

--- src/pages/rahmenprogramm-sportartikel/rahmenprogramm-sportartikel.ts
+++ src/pages/rahmenprogramm-sportartikel/rahmenprogramm-sportartikel.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {VerkaufServiceProvider} from "../../providers/verkauf-service/verkauf-service";
 import {Verkauf} from "../../models/Verkauf";
+import {ApplicationDataServiceProvider} from "../../providers/application-data-service/application-data-service";
 
 /**
  * Generated class for the RahmenprogrammSportartikelPage page.
@@ -19,7 +20,8 @@ export class RahmenprogrammSportartikelPage {
 
   public verkauf = new Verkauf();
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private verkaufService: VerkaufServiceProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private verkaufService: VerkaufServiceProvider,
+              private applicationData:ApplicationDataServiceProvider) {
     verkaufService.getVerkaufObject().subscribe(verkauf => this.verkauf = verkauf);
   }
 
