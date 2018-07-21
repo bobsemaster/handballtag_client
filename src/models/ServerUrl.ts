69bb5544abcc7f372087569d968b7ee883ff81d2
Robert.Schreib <robert.schreib@e-mundo.de>
2018-07-21 08:26:01 +0200

    server url gefixed

--- src/models/ServerUrl.ts
+++ src/models/ServerUrl.ts
@@ -1,3 +1,3 @@
-//export const server_url = "https://handballtag.schreib.io/rest";
-export const server_url = "http://localhost:8080";
+export const server_url = "https://handballtag.schreib.io/rest";
+//export const server_url = "http://localhost:8080";
 
