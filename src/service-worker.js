623e856b9e94eb1751549ce794712677048c3a01
Robert.Schreib <robert.schreib@e-mundo.de>
2019-05-31 20:16:36 +0200

    background notifications einbauen

--- src/service-worker.js
+++ src/service-worker.js
@@ -45,3 +45,16 @@ firebase.initializeApp({
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
 const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.content,
+        icon: '/firebase-logo.png'
+    };
+
+    return self.registration.showNotification(notificationTitle,
+        notificationOptions);
+});
