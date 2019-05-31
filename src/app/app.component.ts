2b295ec42aaced8b5f485a434a6e1d02cb4a6a2e
Robert.Schreib <robert.schreib@e-mundo.de>
2019-05-31 19:00:13 +0200

    Token holen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,7 @@ import {StartPage} from "../pages/start/start";
 import {LageplanPage} from '../pages/lageplan/lageplan';
 import {RahmenprogrammSportartikelPage} from "../pages/rahmenprogramm-sportartikel/rahmenprogramm-sportartikel";
 import {LinksPage} from "../pages/links/links";
+import Messaging = firebase.messaging.Messaging;
 
 @Component({
   templateUrl: 'app.html'
@@ -119,14 +120,41 @@ export class MyApp {
     messaging.usePublicVapidKey("BFaFCZqct4osMzhj4nh_5zs_FtPIWTJtzkkegyWQSO_W92QBVEsSpuQQbEIBfNwJxcyDDELHz2gC-wfiI-QK6I8");
 
 
-    Notification.requestPermission().then(function(permission) {
+    this.getNotificationPermission(messaging);
+  }
+
+  private getNotificationPermission(messaging: Messaging) {
+    Notification.requestPermission().then( (permission) => {
       if (permission === 'granted') {
         console.log('Notification permission granted.');
-        // TODO(developer): Retrieve an Instance ID token for use with FCM.
-        // ...
+// Get Instance ID token. Initially this makes a network call, once retrieved
+// subsequent calls to getToken will return from cache.
+        messaging.getToken().then(currentToken => {
+          if (currentToken) {
+          } else {
+            // Show permission request.
+            console.log('No Instance ID token available. Request permission to generate one.');
+            // Show permission UI.
+          }
+        }).catch(function(err) {
+          console.log('An error occurred while retrieving token. ', err);
+        });
       } else {
         console.log('Unable to get permission to notify.');
       }
+    });
+
+    // Callback fired if Instance ID token is updated.
+    messaging.onTokenRefresh(function() {
+      messaging.getToken().then(function(refreshedToken) {
+        console.log('Token refreshed.');
+        // Indicate that the new Instance ID token has not yet been sent to the
+        // app server.
+        // Send Instance ID token to app server.
+        // ...
+      }).catch(function(err) {
+        console.log('Unable to retrieve refreshed token ', err);
+      });
     });
   }
 }
